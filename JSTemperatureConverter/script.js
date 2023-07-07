c3534717e1bb0826ae25187c1020cc52ee2d5091
Vadim Semenov <semenov@comfortlab.ru>
2023-07-07 11:56:48 +0300

    Add JS temperature converter

--- JSTemperatureConverter/script.js
+++ JSTemperatureConverter/script.js
@@ -1,1 +1,29 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var celsiusTemperatureInput = document.getElementById("celsius_temperature");
+    var kelvinTemperatureInput = document.getElementById("kelvin_temperature");
+    var fahrenheitTemperatureInput = document.getElementById("fahrenheit_temperature");
+    var convertButton = document.getElementById("convert_button");
+
+    convertButton.addEventListener("click", function () {
+        celsiusTemperatureInput.classList.remove("invalid");
+
+        var celsiusTemperature = Number(celsiusTemperatureInput.value.trim());
+
+        if (celsiusTemperatureInput.value.trim().length === 0 || !Number.isInteger(celsiusTemperature)) {
+            celsiusTemperatureInput.classList.add("invalid");
+            return;
+        }
+
+        kelvinTemperatureInput.value = convertCelsiusToKelvin(celsiusTemperature);
+        fahrenheitTemperatureInput.value = convertCelsiusToFahrenheit(celsiusTemperature);
+
+    });
+
+    function convertCelsiusToKelvin(celsiusTemperature) {
+        return celsiusTemperature + 273.15;
+    }
+
+    function convertCelsiusToFahrenheit(celsiusTemperature) {
+        return (celsiusTemperature * (9 / 5)) + 32;
+    }
+});
