e8c742106dab67ab13761dce085d35a552d83377
Vadim Semenov <semenov@comfortlab.ru>
2023-07-18 17:10:13 +0300

    Fix JQuery Todo List

--- JQueryTodoList/script.js
+++ JQueryTodoList/script.js
@@ -1,17 +1,16 @@
-﻿$(function () {
-    var form = $("#todo-form");
-    var todoInput = $("#new-todo-text");
-    var addButton = $("#add-button");
-    var todoList = $("#todo-list");
+﻿$(() => {
+    const form = $("#todo-form");
+    const todoInput = $("#new-todo-text");
+    const addButton = $("#add-button");
+    const todoList = $("#todo-list");
 
-
-    form.submit(function (e) {
+    form.submit((e) => {
         e.preventDefault();
     });
 
-    addButton.click(function () {
+    addButton.click(() => {
 
-        var todoText = todoInput.val().trim();
+        let todoText = todoInput.val().trim();
         todoInput.removeClass("invalid");
 
         if (todoText.length === 0) {
@@ -19,7 +18,7 @@
             return;
         }
 
-        var todoItem = $("<li class='row'>").addClass("todo_item");
+        const todoItem = $("<li class='row'>").addClass("todo_item");
 
         setViewMode();
 
@@ -30,34 +29,34 @@
         function setViewMode() {
 
             todoItem.html("<div class='col todo_item_text'></div>\
-            <div class='col button_content_block'>\
-        <button class ='btn edit_button small_button' type='button'>Изменить</button>\
-            <button class='btn delete_button small_button' type='button'>Удалить</button>\
-                </div>");
+                           <div class='col button_content_block'>\
+                                <button class ='btn edit_button small_button' type='button'>Изменить</button>\
+                                <button class='btn delete_button small_button delete_button' type='button'>Удалить</button>\
+                           </div>");
 
             todoItem.find(".todo_item_text").text(todoText);
 
-            todoItem.find(".delete_button").click(function () {
+            todoItem.find(".delete_button").click(() => {
                 todoItem.remove();
             });
 
-            todoItem.find(".edit_button").click(function () {
+            todoItem.find(".edit_button").click(() => {
                 setEditMode();
             });
-        } 
+        }
 
         function setEditMode() {
             todoItem.html("<input class='form-control edit_todo_item'>\
-            <div class='error_message'>Строка не должна быть пустой!</div>\
-            <div class='content_block'>\
-        <button class ='btn save_button small_button' type='button'>Сохранить</button>\
-            <button class='btn cancel_button small_button' type='button'>Выйти</button>\
-            </div>");
+                           <div class='error_message'>Строка не должна быть пустой!</div>\
+                           <div class='content_block'>\
+                                <button class ='btn save_button small_button' type='button'>Сохранить</button>\
+                                <button class='btn cancel_button small_button exit_button' type='button'>Выйти</button>\
+                           </div>");
 
             todoItem.find(".edit_todo_item").val(todoText);
 
-            todoItem.find(".save_button").click(function () {
-                var editedTodoText = todoItem.find(".edit_todo_item").val().trim();
+            todoItem.find(".save_button").click(() => {
+                const editedTodoText = todoItem.find(".edit_todo_item").val().trim();
 
                 if (editedTodoText.length === 0) {
                     todoItem.find(".edit_todo_item").addClass("invalid");
@@ -70,7 +69,7 @@
                 }
             });
 
-            todoItem.find(".cancel_button").click(function () {
+            todoItem.find(".cancel_button").click(() => {
                 setViewMode();
             });
         }
