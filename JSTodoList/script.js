a9036a988385ab6fd225fa76bfc6c23cb0157429
Vadim Semenov <semenov@bimdance.io>
2023-11-19 15:45:09 +0300

    Fix JS TodoList

--- JSTodoList/script.js
+++ JSTodoList/script.js
@@ -4,12 +4,9 @@
     const addButton = document.getElementById("add-button");
     const todoList = document.getElementById("todo-list");
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-    });
+    form.addEventListener("submit", e => e.preventDefault());
 
     addButton.addEventListener("click", () => {
-
         let todoText = todoInput.value.trim();
         todoInput.classList.remove("invalid");
 
@@ -31,32 +28,38 @@
         function setViewMode() {
             todoItem.innerHTML = "<div class='todo_item_text'></div>\
                                   <div class='content_block'>\
-                                     <button class ='edit_button small_button' type='button'>Изменить</button>\
+                                     <button class='edit_button small_button' type='button'>Изменить</button>\
                                      <button class='delete_button small_button delete_button' type='button'>Удалить</button>\
                                   </div>";
 
             todoItem.querySelector(".todo_item_text").textContent = todoText;
 
-            todoItem.querySelector(".delete_button").addEventListener("click", function () {
-                todoItem.remove();
-            });
+            todoItem.querySelector(".delete_button").addEventListener("click", () => todoItem.remove());
 
-            todoItem.querySelector(".edit_button").addEventListener("click", function () {
-                setEditMode();
-            });
+            todoItem.querySelector(".edit_button").addEventListener("click", () => setEditMode());
         }
 
         function setEditMode() {
             todoItem.innerHTML = "<input class='edit_todo_item'>\
                                   <div class='error_message'>Строка не должна быть пустой!</div>\
                                   <div class='content_block'>\
-                                       <button class ='save_button small_button' type='button'>Сохранить</button>\
+                                       <button class='save_button small_button' type='button'>Сохранить</button>\
                                        <button class='cancel_button small_button exit_button' type='button'>Выйти</button>\
                                   </div>";
 
             todoItem.querySelector(".edit_todo_item").value = todoText;
 
-            todoItem.querySelector(".save_button").addEventListener("click", function () {
+            todoItem.querySelector(".edit_todo_item").addEventListener("keydown", event => {
+                if (event.key === 13 || event.key === "Enter") {
+                    saveEvent();
+                }
+            });
+
+            todoItem.querySelector(".save_button").addEventListener("click", () => saveEvent());
+
+            todoItem.querySelector(".cancel_button").addEventListener("click", () => setViewMode());
+
+            function saveEvent() {
                 const editedTodoText = todoItem.querySelector(".edit_todo_item").value.trim();
 
                 if (editedTodoText.length === 0) {
@@ -68,11 +71,7 @@
 
                     setViewMode();
                 }
-            });
-
-            todoItem.querySelector(".cancel_button").addEventListener("click", () => {
-                setViewMode();
-            });
+            }
         }
     });
 });
