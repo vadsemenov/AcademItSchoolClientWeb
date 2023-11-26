4953ad5b72739bd0462a303eb354c81e190ceb68
Vadim Semenov <semenov@bimdance.io>
2023-11-26 18:40:55 +0300

    Fix JSWorkWithObjects

--- JSWorkWithObjects/script.js
+++ JSWorkWithObjects/script.js
@@ -1,57 +1,60 @@
-﻿const countries = [
-    {
-        name: "Швейцария",
-        cities: [
-            { name: "Цюрих", population: 378884 },
-            { name: "Женева", population: 188634 },
-            { name: "Базель", population: 164937 }
-        ]
-    },
-    {
-        name: "Китай",
-        cities: [
-            { name: "Чунцин", population: 30165500 },
-            { name: "Шанхай", population: 24150000 },
-            { name: "Пекин", population: 21148000 }
-        ]
-    },
-    {
-        name: "Австралия",
-        cities: [
-            { name: "Мельбурн", population: 4529500 },
-            { name: "Брисбен", population: 2308700 },
-            { name: "Сидней", population: 4921000 },
-            { name: "Перт", population: 2039200 }
-        ]
+﻿(function () {
+    const countries = [
+        {
+            name: "Швейцария",
+            cities: [
+                { name: "Цюрих", population: 378884 },
+                { name: "Женева", population: 188634 },
+                { name: "Базель", population: 164937 }
+            ]
+        },
+        {
+            name: "Китай",
+            cities: [
+                { name: "Чунцин", population: 30165500 },
+                { name: "Шанхай", population: 24150000 },
+                { name: "Пекин", population: 21148000 }
+            ]
+        },
+        {
+            name: "Австралия",
+            cities: [
+                { name: "Мельбурн", population: 4529500 },
+                { name: "Брисбен", population: 2308700 },
+                { name: "Сидней", population: 4921000 },
+                { name: "Перт", population: 2039200 }
+            ]
+        }
+    ];
+
+    function getCountriesWithMaxCitiesCount(countriesArray) {
+        const maxCitiesCount = countriesArray.reduce(
+            (maxCount, country) => Math.max(country.cities.length, maxCount),
+            0);
+
+        return countriesArray.filter(country => country.cities.length === maxCitiesCount);
     }
-];
 
-function getCountriesWithMaxCitiesCount(countriesArray) {
-    const maxCitiesCount = countriesArray.reduce(
-        (maxCount, country) => Math.max(country.cities.length, maxCount),
-        0);
+    function getCountriesPopulation(countriesArray) {
+        const countriesPopulation = {};
 
-    return countriesArray.filter(country => country.cities.length === maxCitiesCount);
-}
+        countriesArray.forEach(country => {
+            countriesPopulation[country.name] = country.cities.reduce((populationSum, city) => {
+                return populationSum + city.population;
+            }, 0);
+        });
 
-function getCountriesPopulation(countriesArray) {
-    const countriesPopulation = {};
-
-    countriesArray.forEach(country => {
-        countriesPopulation[country.name] = country.cities.reduce((populationSum, city) => {
-            return populationSum + city.population;
-        }, 0);
-    });
-
-    return countriesPopulation;
-}
-
-const countriesWithMaxCitiesCount = getCountriesWithMaxCitiesCount(countries);
-console.log("Страны с большим количеством городов: " + countriesWithMaxCitiesCount.map(country => country.name).join(" "));
-
-const countriesPopulation = getCountriesPopulation(countries);
-for (let field in countriesPopulation) {
-    if (countriesPopulation.hasOwnProperty(field)) {
-        console.log("Страна - " + field + ": население - " + countriesPopulation[field] + " человек");
+        return countriesPopulation;
     }
-}
+
+    const countriesWithMaxCitiesCount = getCountriesWithMaxCitiesCount(countries);
+    console.log("Страны с большим количеством городов: " + countriesWithMaxCitiesCount.map(country => country.name).join(" "));
+
+    const countriesPopulation = getCountriesPopulation(countries);
+
+    for (let countryName in countriesPopulation) {
+        if (countriesPopulation.hasOwnProperty(countryName)) {
+            console.log("Страна - " + countryName + ": население - " + countriesPopulation[countryName] + " человек");
+        }
+    }
+})();
