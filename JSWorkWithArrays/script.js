e7a3f2c150aa8799afce94658d5a1eca151f1677
Vadim Semenov <semenov@bimdance.io>
2023-11-26 18:34:29 +0300

    Fix JSWorkWithArray

--- JSWorkWithArrays/script.js
+++ JSWorkWithArrays/script.js
@@ -1,50 +1,54 @@
-﻿function getSortedArrayOrderedByDescending(array) {
-    return array.sort((e1, e2) => e2 - e1);
-}
-
-function getArrayFirstFiveElements(array) {
-    return array.slice(0, 5);
-}
-
-function getArrayLastFiveElements(array) {
-    return array.slice(-5);
-}
-
-function getEvenElementsSum(array) {
-    const evenElementsArray = array.filter(element => element % 2 === 0);
-
-    return evenElementsArray.reduce((currentSum, currentNumber) => currentSum + currentNumber, 0);
-}
-
-function getArrayFrom1To100() {
-    const array = [];
-
-    for (let i = 1; i <= 100; i++) {
-        array.push(i);
+﻿(function () {
+    function sortArrayOrderByDescending(array) {
+        array.sort((e1, e2) => e2 - e1);
     }
 
-    return array;
-}
+    function getArrayFirstFiveElements(array) {
+        return array.slice(0, 5);
+    }
 
-function getEvenNumbersSquaresList() {
-    const array = getArrayFrom1To100();
+    function getArrayLastFiveElements(array) {
+        return array.slice(-5);
+    }
 
-    return array
-        .filter(item => item % 2 === 0)
-        .map(item => item * item);
-}
+    function getEvenElementsSum(array) {
+        return array.filter(element => element % 2 === 0)
+            .reduce((currentSum, currentNumber) => currentSum + currentNumber, 0);
+    }
 
-var sortedArray = getSortedArrayOrderedByDescending([2, 3, 4, 1]);
-console.log("Отсортированный массив по убыванию: " + sortedArray.join(" "));
+    function getFrom1To100Array() {
+        const array = [];
 
-var arrayFirstFiveElements = getArrayFirstFiveElements([0, 1, 2, 3, 4, 5, 6]);
-console.log("Первые 5 чисел в массиве: " + arrayFirstFiveElements.join(" "));
+        for (let i = 1; i <= 100; i++) {
+            array.push(i);
+        }
 
-var arrayLastFiveElements = getArrayLastFiveElements([0, 1, 2, 3, 4, 5, 6]);
-console.log("Последние 5 чисел в массиве: " + arrayLastFiveElements.join(" "));
+        return array;
+    }
 
-var evenElementsSum = getEvenElementsSum([0, 1, 2, 3, 4, 5, 6]);
-console.log("Сумма четных элементов массива = " + evenElementsSum);
+    function getEvenNumbersSquaresArray(array) {
+        //        const array = getFrom1To100Array();
+        return array
+            .filter(item => item % 2 === 0)
+            .map(item => item * item);
+    }
 
-var evenNumbersSquaresList = getEvenNumbersSquaresList();
-console.log("Список квадратов четных чисел: " + evenNumbersSquaresList.join(" "));
+    const array1 = [2, 3, 4, 1];
+    const array2 = [0, 1, 2, 3, 4, 5, 6];
+    const array3 = getFrom1To100Array();
+
+    sortArrayOrderByDescending(array1);
+    console.log("Отсортированный массив по убыванию: " + array1.join(" "));
+
+    const arrayFirstFiveElements = getArrayFirstFiveElements(array2);
+    console.log("Первые 5 чисел в массиве: " + arrayFirstFiveElements.join(" "));
+
+    const arrayLastFiveElements = getArrayLastFiveElements(array2);
+    console.log("Последние 5 чисел в массиве: " + arrayLastFiveElements.join(" "));
+
+    const evenElementsSum = getEvenElementsSum(array2);
+    console.log("Сумма четных элементов массива = " + evenElementsSum);
+
+    const evenNumbersSquaresList = getEvenNumbersSquaresArray(array3);
+    console.log("Список квадратов четных чисел: " + evenNumbersSquaresList.join(" "));
+})();
