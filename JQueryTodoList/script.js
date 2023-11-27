9c9f53a0257b17e09c4ca664dca80246da460baf
Vadim Semenov <semenov@bimdance.io>
2023-11-27 19:43:40 +0300

    Fix JqueryTodo List (Bootstrap)

--- JQueryTodoList/script.js
+++ JQueryTodoList/script.js
@@ -17,7 +17,7 @@
             return;
         }
 
-        const todoItem = $("<li class='row list-group-item d-flex'>");
+        const todoItem = $("<li class='list-group-item d-flex justify-content-between'>");
 
         setViewMode();
 
@@ -26,57 +26,64 @@
         todoInput.val("");
 
         function setViewMode() {
-            todoItem.html("<div class='col-9 todo_item_text text-justify'></div>\
-                            <div class= 'col-2 btn-group' role ='group' >\
-                                 <button class='btn btn-success edit_button' type='button'>Изменить</button>\
-                                 <button class='btn btn-danger delete_button' type='button'>Удалить</button>\
-                            </div>");
+            todoItem.html("<div class='col todo-item-text text-start text-break'></div>\
+                           <div class='col-3 text-end'>\
+                                 <button class='btn btn-success edit-button button-width' type='button'>Изменить</button>\
+                                 <button class='btn btn-danger delete-button button-width' type='button'>Удалить</button>\
+                           </div>");
 
-            todoItem.find(".todo_item_text").text(todoText);
+            todoItem.find(".todo-item-text").text(todoText);
 
-            todoItem.find(".delete_button").click(() => todoItem.remove());
+            todoItem.find(".delete-button").click(() => todoItem.remove());
 
-            todoItem.find(".edit_button").click(() => setEditMode());
+            todoItem.find(".edit-button").click(() => setEditMode());
         }
 
         function setEditMode() {
             todoItem.html("<div class='input-group has-validation'>\
-                               <input class='col form-control edit_todo_item'>\
-                               <div class='col btn-group' role='group'>\
-                                   <button class ='btn btn-success save_button' type='button'>Сохранить</button>\
-                                   <button class='btn btn-secondary cancel_button' type='button'>Выйти</button>\
-                               </div>\
+                             <div class='col'>\
+                               <input class='form-control edit-todo-item'>\
                                <div class='invalid-feedback'>\
                                    Строка не должна быть пустой!\
-                               </div >\
+                               </div>\
+                             </div>\
+                             <div class='col-3 text-end'>\
+                                   <button class='btn btn-success save-button button-width' type='button'>Сохранить</button>\
+                                   <button class='btn btn-secondary cancel-button button-width' type='button'>Выйти</button>\
+                             </div>\
                            </div>");
 
-            const todoItemInput = todoItem.find(".edit_todo_item");
+            const todoItemInput = todoItem.find(".edit-todo-item");
 
             todoItemInput.val(todoText);
 
             todoItemInput.keypress(event => {
                 if (event.key === 13 || event.key === "Enter") {
+                    event.preventDefault();
                     saveEventHandler();
                 }
             });
 
-            todoItem.find(".save_button").click(() => saveEventHandler());
+            todoItem.find(".save-button").click(() => saveEventHandler());
 
-            todoItem.find(".cancel_button").click(() => setViewMode());
+            todoItem.find(".cancel-button").click(() => setViewMode());
 
             function saveEventHandler() {
-                const editedTodoText = todoItem.find(".edit_todo_item").val().trim();
+                const editedTodoText = todoItem.find(".edit-todo-item").val().trim();
 
                 if (editedTodoText.length === 0) {
-                    todoItem.find(".edit_todo_item").addClass("is-invalid");
-                } else {
-                    todoItem.find(".edit_todo_item").removeClass("is-invalid");
+                    todoItem.find(".edit-todo-item").addClass("is-invalid");
 
-                    todoText = editedTodoText;
-
-                    setViewMode();
+                    return;
                 }
+
+                todoItem.find(".edit-todo-item").removeClass("is-invalid");
+
+                todoText = editedTodoText;
+
+                setViewMode();
+
+                return;
             }
         }
     });
