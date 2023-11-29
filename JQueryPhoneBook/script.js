8d477619d28962f295f40a787338f670fd444c92
Vadim Semenov <semenov@bimdance.io>
2023-11-29 22:04:25 +0300

    Fix Jquery Phone Book

--- JQueryPhoneBook/script.js
+++ JQueryPhoneBook/script.js
@@ -1,25 +1,77 @@
-﻿$(function () {
-    var addForm = $("#add_form");
-    var contactsForm = $("#contacts_form");
-    var deleteButton = $("#delete_button");
+﻿$(document).ready(function () {
+    const addForm = $("#add-form");
+    const addButton = $("#add-button");
+    const tbodyBlock = $("#tbody-block");
+    const phoneNumberInput = $("#phone-add-input");
+
+    addButton.click(addContactToTable);
+
+    phoneNumberInput.on("input", function (event) {
+        const eventInput = $(event.target);
+        const text = eventInput.val();
+
+        if (text.length > 12) {
+            eventInput.val(text.slice(0, 12));
+        }
+    });
 
     addForm.submit(function (e) {
         e.preventDefault();
     });
 
-    contactsForm.submit(function(e) {
-        e.preventDefault();
-    });
+    function addContactToTable() {
+        if (!validateInputText()) {
+            return;
+        }
 
-    deleteButton.click(function () {
+        const firstName = $("#first-name-add-input");
+        const lastName = $("#last-name-add-input");
+        const phoneNumber = $("#phone-add-input");
 
-        $("#dialog_confirm").dialog({
+        const tableRow = $("<tr class='table-row'></tr>");
+
+        const numberCell = $("<td class='number-cell'></td>").text("Number");
+        const lastNameCell = $("<td class='last-name-cell'></td>").text(lastName.val());
+        const firstNameCell = $("<td class='first-name-cell'></td>").text(firstName.val());
+        const phoneNumberCell = $("<td class='phone-cell'></td>").text(phoneNumber.val());
+
+        const deleteRowButton = $("<input class='delete-row-button' type='button' name='delete-contact' value='X' title='Delete this row' />")
+            .click(showDeleteDialog);
+
+        const buttonCell = $("<td></td>").append(deleteRowButton);
+
+        tableRow.append(numberCell);
+        tableRow.append(lastNameCell);
+        tableRow.append(firstNameCell);
+        tableRow.append(phoneNumberCell);
+        tableRow.append(buttonCell);
+
+        tbodyBlock.append(tableRow);
+
+        numerateContacts();
+    }
+
+    function showDeleteDialog(event) {
+        const rowForDelete = $(event.target).closest(".table-row");
+
+        const lastName = rowForDelete.find(".last-name-cell").text();
+        const firstName = rowForDelete.find(".first-name-cell").text();
+        const phoneNumber = rowForDelete.find(".phone-cell").text();
+
+        const dialog = $("#dialog-confirm");
+
+        dialog.find(".first-name-dialog").text(firstName);
+        dialog.find(".last-name-dialog").text(lastName);
+        dialog.find(".phone-number-dialog").text(phoneNumber);
+
+        dialog.dialog({
             resizable: false,
             height: "auto",
             width: 400,
             modal: true,
             buttons: {
-                "Delete all items": function () {
+                "Delete contact": function () {
+                    deleteContactRow(event);
                     $(this).dialog("close");
                 },
                 Cancel: function () {
@@ -27,69 +79,45 @@
                 }
             }
         });
+    }
 
-//        var todoText = todoInput.val().trim();
-//        todoInput.removeClass("invalid");
-//
-//        if (todoText.length === 0) {
-//            todoInput.addClass("invalid");
-//            return;
-//        }
-//
-//        var todoItem = $("<li>").addClass("todo_item");
-//
-//        setViewMode();
-//
-//        todoList.append(todoItem);
-//
-//        todoInput.val("");
-//
-//        function setViewMode() {
-//
-//            todoItem.html("<div class='todo_item_text'></div>\
-//            <div class='content_block'>\
-//        <button class ='edit_button small_button' type='button'>Изменить</button>\
-//            <button class='delete_button small_button' type='button'>Удалить</button>\
-//                </div>");
-//
-//            todoItem.find(".todo_item_text").text(todoText);
-//
-//            todoItem.find(".delete_button").click(function () {
-//                todoItem.remove();
-//            });
-//
-//            todoItem.find(".edit_button").click(function () {
-//                setEditMode();
-//            });
-//        }
-//
-//        function setEditMode() {
-//            todoItem.html("<input class='edit_todo_item'>\
-//            <div class='error_message'>Строка не должна быть пустой!</div>\
-//            <div class='content_block'>\
-//        <button class ='save_button small_button' type='button'>Сохранить</button>\
-//            <button class='cancel_button small_button' type='button'>Выйти</button>\
-//            </div>");
-//
-//            todoItem.find(".edit_todo_item").val(todoText);
-//
-//            todoItem.find(".save_button").click(function () {
-//                var editedTodoText = todoItem.find(".edit_todo_item").val().trim();
-//
-//                if (editedTodoText.length === 0) {
-//                    todoItem.find(".edit_todo_item").addClass("invalid");
-//                } else {
-//                    todoItem.find(".edit_todo_item").removeClass("invalid");
-//
-//                    todoText = editedTodoText;
-//
-//                    setViewMode();
-//                }
-//            });
-//
-//            todoItem.find(".cancel_button").click(function () {
-//                setViewMode();
-//            });
-//        }
-    });
+    function deleteContactRow(event) {
+        const rowForDelete = $(event.target).closest(".table-row");
+
+        rowForDelete.remove();
+
+        numerateContacts();
+    }
+
+    function numerateContacts() {
+        const rowNumberCells = $(".number-cell");
+
+        if (rowNumberCells.length === 0) {
+            return;
+        }
+
+        rowNumberCells.each(function (index) {
+            $(this).text(index + 1);
+        });
+    }
+
+    function validateInputText() {
+        const inputs = $(".input-block");
+        let allInputsIsValid = true;
+
+        inputs.each(function (index) {
+            const input = $(this);
+            const errorText = input.next();
+
+            if (input.val().length === 0) {
+                errorText.css("display", "block");
+                errorText.css("color", "#f00");
+                allInputsIsValid = false;
+            } else {
+                errorText.css("display", "none");
+            }
+        });
+
+        return allInputsIsValid;
+    }
 });
