6d824837248e34e03d905c3c22609003937d2df3
Vadim Semenov <semenov@bimdance.io>
2023-12-28 21:36:14 +0300

    Fix errors without edit dialog

--- Vue3PhoneBook/script.js
+++ Vue3PhoneBook/script.js
@@ -9,11 +9,47 @@
             firstName: "",
             phoneNumber: "",
             contactId: 1,
-            numberPattern: /^([+]?[0-9\s-\(\)]{3,25})*$/i
-        }
+            phoneNumberPattern: /^([+]?[0-9\s-\(\)]{3,25})*$/i,
+            editModalDialog: null,
+            deleteModalDialog: null,
+            contactToDelete: "",
+            contactToEdit: ""
+        };
     },
 
+    mounted() {
+        this.setFocusToLastName();
+
+        this.deleteModalDialog = new bootstrap.Modal(this.$refs.deleteContactModalDialog);
+        this.editModalDialog = new bootstrap.Modal(this.$refs.editContactModalDialog);
+    },
+
+
     methods: {
+        setFocusToLastName: function () {
+            this.$refs.lastName.focus();
+        },
+
+        showDeleteContactModalDialog(contact) {
+            this.contactToDelete = contact;
+
+            this.deleteModalDialog.show();
+        },
+
+        hideDeleteContactModalDialog() {
+            this.deleteModalDialog.hide();
+        },
+
+        showEditContactModalDialog(contact) {
+            this.contactToEdit = contact;
+
+            this.editModalDialog.show();
+        },
+
+        hideEditContactModalDialog() {
+            this.editModalDialog.hide();
+        },
+
         addContact() {
             if (!this.validateAllFields()) {
                 return;
@@ -75,13 +111,15 @@
                 return false;
             }
 
-            return this.numberPattern.test(text);
+            return this.phoneNumberPattern.test(text);
         },
 
         deleteContact(index) {
+            this.hideDeleteContactModalDialog();
+
             this.contacts.splice(index, 1);
         }
     }
-}
+};
 
-Vue.createApp(Vue3PhoneBook).mount('#app');
+Vue.createApp(Vue3PhoneBook).mount("#app");
